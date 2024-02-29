e130b8ab852942c1c1ffc4c1121f91f21eacbada
Roman Troshin <troshinrabota@gmail.com>
2024-02-29 16:45:58 +0300

    Реализовать Пагинацию на главной странице

--- my-app/src/bff/operations/fetch-posts.js
+++ my-app/src/bff/operations/fetch-posts.js
@@ -1,14 +1,43 @@
 import { getComments, getPosts } from '../api';
 import { getCommentsCount } from '../utils';
 
-export const fetchPosts = async () => {
-	const [posts, comments] = await Promise.all([getPosts(), getComments()]);
+export const fetchPosts = async (page, limit) => {
+	const [{ posts, links }, comments] = await Promise.all([
+		getPosts(page, limit),
+		getComments(),
+	]);
 
 	return {
 		error: null,
-		res: posts.map((post) => ({
-			...post,
-			commentsCount: getCommentsCount(comments, post.id),
-		})),
+		res: {
+			posts: posts.map((post) => ({
+				...post,
+				commentsCount: getCommentsCount(comments, post.id),
+			})),
+			links,
+		},
 	};
 };
+
+
+/*
+export const fetchPosts = async (page, limit) => {
+	const [{ posts, links }, comments] = await Promise.all([
+		getPosts(page, limit),
+		getComments(),
+	]);
+	console.log('links:', links);
+	console.log('posts:', posts)
+
+	return {
+		error: null,
+		res: {
+			posts: posts.map((post) => ({
+				...post,
+				commentsCount: getCommentsCount(comments, post.id),
+				links,
+			})),
+		},
+	};
+};
+*/
--- my-app/src/pages/main/main.js
+++ my-app/src/pages/main/main.js
@@ -1,22 +1,29 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { PostCard } from './components';
+import { Pagination, PostCard } from './components';
 import { useServerRequest } from '../../hooks';
+import { PAGINATION_LIMIT } from '../../constants';
+import { getLastPageFromLinks } from './utils/get-last-page-from-links';
 
 const MainContainer = ({ className }) => {
 	const [posts, setPosts] = useState([]);
+	const [page, setPage] = useState(1);
+	const [lastPage, setLastPage] = useState(1);
 	const requestServer = useServerRequest();
 
 	useEffect(() => {
-		requestServer('fetchPosts').then((posts) => {
-			setPosts(posts.res);
-		});
-	}, [requestServer]);
+		requestServer('fetchPosts', page, PAGINATION_LIMIT).then(
+			({ res: { posts, links } }) => {
+				setPosts(posts);
+				setLastPage(getLastPageFromLinks(links));
+			},
+		);
+	}, [requestServer, page]);
 
 	return (
 		<div className={className}>
 			<div className="post-list">
-				{posts.map(({ imageUrl, id, title, publishedAt, commentsCount }) => (
+				{posts.map(({ id, title, imageUrl, publishedAt, commentsCount }) => (
 					<PostCard
 						key={id}
 						id={id}
@@ -27,6 +34,7 @@ const MainContainer = ({ className }) => {
 					/>
 				))}
 			</div>
+			{lastPage > 1 && <Pagination page={page} lastPage={lastPage} setPage={setPage} />}
 		</div>
 	);
 };
@@ -37,5 +45,51 @@ export const Main = styled(MainContainer)`
 		flex-wrap: wrap;
 		padding: 20px;
 	}
-
 `;
+
+
+
+/*
+const MainContainer = ({ className }) => {
+	const [posts, setPosts] = useState([]);
+	const [page, setPage] = useState(1);
+	const [lastPage, setLastPage] = useState(1);
+	const requestServer = useServerRequest();
+
+	useEffect(() => {
+		requestServer('fetchPosts', page, PAGINATION_LIMIT).then(
+			({ res: { posts, links } }) => {
+				setPosts(posts);
+				setLastPage(getLastPageFromLinks(links));
+			},
+		);
+	}, [requestServer, page]);
+
+	return (
+		<div className={className}>
+			<div className="post-list">
+				{posts.map(({ id, title, imageUrl, publishedAt, commentsCount }) => (
+					<PostCard
+						key={id}
+						id={id}
+						title={title}
+						imageUrl={imageUrl}
+						publishedAt={publishedAt}
+						commentsCount={commentsCount}
+					/>
+				))}
+			</div>
+			{lastPage > 1 && <Pagination page={page} lastPage={lastPage} setPage={setPage} />}{' '}
+		</div>
+	);
+};
+
+export const Main = styled(MainContainer)`
+	& .post-list {
+		display: flex;
+		flex-wrap: wrap;
+		padding: 20px;
+	}
+`;
+
+*/
